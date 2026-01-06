d2333d2337467d54d22994c1da661eea88042d10
jagrajsingh131 <143749320+jagrajsingh131@users.noreply.github.com>
2026-01-06 13:42:58 +1100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMYpdPB9qheEGc1BlalSAZC1giaHSnbpE",
@@ -9,4 +10,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
