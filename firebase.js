1e560ade2c61dbe34a80ea1cb9bad9498b6444da
jagrajsingh131 <143749320+jagrajsingh131@users.noreply.github.com>
2026-01-06 15:13:11 +1100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBMYpdPB9qheEGc1BlalSAZC1giaHSnbpE",
   authDomain: "buckupmedia-6ad96.firebaseapp.com",
   projectId: "buckupmedia-6ad96",
-  storageBucket: "buckupmedia-6ad96.firebasestorage.app",
+  storageBucket: "buckupmedia-6ad96.appspot.com",
   messagingSenderId: "931014030006",
   appId: "1:931014030006:web:dda47360939201a1a8ec17",
-  measurementId: "G-1CJCCQ2B5C"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
