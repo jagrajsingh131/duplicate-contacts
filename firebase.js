244e9acf7bafb6dc27cad58a6ed17613772ad7df
jagrajsingh131 <143749320+jagrajsingh131@users.noreply.github.com>
2026-01-06 13:33:26 +1100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBMYpdPB9qheEGc1BlalSAZC1giaHSnbpE",
   authDomain: "buckupmedia-6ad96.firebaseapp.com",
   projectId: "buckupmedia-6ad96",
-  storageBucket: "buckupmedia-6ad96.firebasestorage.app",
-  messagingSenderId: "931014030006",
   appId: "1:931014030006:web:dda47360939201a1a8ec17",
-  measurementId: "G-1CJCCQ2B5C"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
